9d0c245569631960ac34b72265cbd21905df8735
Salman-Irfan <salmanirfan674@gmail.com>
2023-09-02 02:56:52 -0700

    switch statements

---
+++ 9switch.php
@@ -0,0 +1,23 @@
+<?php
+$age = 25;
+switch($age){
+    case 10:{
+        echo "You are 10 years old <br/>";
+        echo "You are 10 years old <br/>";
+        break;
+    }
+    case 20:{
+        echo "You are 20 years old <br/>";
+        echo "You are 20 years old <br/>";
+        break;
+    }
+    case 30:{
+        echo "You are 30 years old <br/>";
+        echo "You are 30 years old <br/>";
+        break;
+    }
+    default:{
+        echo "You are 40 years old <br/>";
+    }
+}
+?>
