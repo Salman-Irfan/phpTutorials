333844924ce011126743b30ef92c38c30e5ff58c
Salman-Irfan <salmanirfan674@gmail.com>
2023-09-02 01:08:16 -0700

    datatypes in php

---
+++ 5.dataTypes.php
@@ -0,0 +1,18 @@
+<?php
+    // string
+    // integer
+    // float
+    // boolean
+    // array
+    // object
+    // NULL
+    // array
+    $friends = array();
+    $friends[0] = "salman";
+    echo var_dump($friends);
+    echo "<br>";
+    $friendlist = array("salman", "zeeshan", "hamza", "azam");
+    echo var_dump($friendlist);
+    echo "<br>";
+    echo $friendlist[2];
+?>
