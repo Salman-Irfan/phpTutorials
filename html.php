9089295bd194ef17cbcd625bc547d014d8a334d4
Salman-Irfan <salmanirfan674@gmail.com>
2023-09-02 00:22:45 -0700

    first commit

---
+++ html.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Php Tag Title";?></title>
+</head>
+<body>
+    <?php
+        echo "<h1>Php Tag Title</h1>";
+    ?>
+</body>
+</html>
